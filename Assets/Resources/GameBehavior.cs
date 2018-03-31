4122370c52de6b692b6e6fdb896f04635d9b4d8e
xsmiledur <xsmiledur.8x7@gmail.com>
2018-03-31 21:12:20 +0900

    タップしたときカーソル位置でオブジェクトが出るように修正（タップする予定はないだろうけど）

--- Assets/Resources/GameBehavior.cs
+++ Assets/Resources/GameBehavior.cs
@@ -5,31 +5,24 @@ using UnityEngine.XR.WSA.Input;
 public class GameBehavior : MonoBehaviour {
 
     public GameObject _TestBox;
-    private GameObject[] TestBox;
+    private GameObject TestBox;
+    private GameObject cursor;
     private GestureRecognizer gestureRecognizer;
-    private GazeManager gazeManager;
 
     // Use this for initialization
     void Start () {
-        // initialize TestBox
-        TestBox = new GameObject[3];
         // load TestBoxPrefab
         _TestBox = (GameObject)Resources.Load("TestBox");
-        // initialize GazeManager
-        gazeManager = new GazeManager();
+        // initialize Cursor
+        cursor = GameObject.Find("DefaultCursor").gameObject;
         // initialize GestureRecognizer
         gestureRecognizer = new GestureRecognizer();
         gestureRecognizer.Tapped += (TappedEventArgs args) =>
         {
             Debug.Log("TappedEvent");
-           
             // if be notificated from image processing, an object is generated
-            if (TestBox[2] != null)
-            {
-                Destroy(TestBox[2]); TestBox[2] = null;
-            }
-            for (int i = 0; i < 2; i++) TestBox[i + 1] = TestBox[i];
-            TestBox[0] = Instantiate(_TestBox, gazeManager.GazeNormal, new Quaternion());
+            if (TestBox != null) Destroy(TestBox);
+            TestBox = Instantiate(_TestBox, cursor.transform.position, new Quaternion());
         };
         gestureRecognizer.StartCapturingGestures();
 
