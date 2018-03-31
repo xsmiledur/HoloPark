8b58eee83a5308037eb34e76a0fa57a7310847cd
xsmiledur <xsmiledur.8x7@gmail.com>
2018-03-31 20:01:08 +0900

    微修正

--- Assets/Resources/GameBehavior.cs
+++ Assets/Resources/GameBehavior.cs
@@ -1,39 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using HoloToolkit.Unity.InputModule;
 using UnityEngine.XR.WSA.Input;
-using UnityEngine.EventSystems;
 
-//public class GameBehavior : MonoBehaviour, IInputClickHandler
 public class GameBehavior : MonoBehaviour {
 
     public GameObject _TestBox;
     private GameObject[] TestBox;
     private GestureRecognizer gestureRecognizer;
     private GazeManager gazeManager;
-    private FocusManager focusManager;
-
-    /** クリック時の挙動 */
-    //public void OnInputClicked(InputClickedEventData eventData)
-    //{
-    //    // if be notificated from image processing, an object is generated
-    //    if (TestBox[2] != null) {
-    //        Destroy(TestBox[2]); TestBox[2] = null;
-    //    }
-    //    for (int i = 0; i < 2; i++) TestBox[i + 1] = TestBox[i];
-    //    TestBox[0] = Instantiate(_TestBox);
-    //    Debug.LogFormat("OnInputClicked\r\nSource: {0}  SourceId: {1} TapCount: {2}", eventData.InputSource, eventData.SourceId, eventData.TapCount);
-    //}
-
-    //public void GestureRecognizer_Tapped(TappedEventArgs args)
-    //{
-    //    Debug.Log("GestureRecognizer Tapped");
-    //}
-    //public void OnTappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
-    //{
-    //    Debug.Log("source:aaa ");
-    //}
 
     // Use this for initialization
     void Start () {
@@ -48,14 +22,14 @@ public class GameBehavior : MonoBehaviour {
         gestureRecognizer.Tapped += (TappedEventArgs args) =>
         {
             Debug.Log("TappedEvent");
+           
             // if be notificated from image processing, an object is generated
             if (TestBox[2] != null)
             {
                 Destroy(TestBox[2]); TestBox[2] = null;
             }
             for (int i = 0; i < 2; i++) TestBox[i + 1] = TestBox[i];
-            Vector3 position = gazeManager.GazeNormal;
-            TestBox[0] = Instantiate(_TestBox, position, new Quaternion());
+            TestBox[0] = Instantiate(_TestBox, gazeManager.GazeNormal, new Quaternion());
         };
         gestureRecognizer.StartCapturingGestures();
 
