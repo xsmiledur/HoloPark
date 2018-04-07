c5bb6d22e5e93a5c0ef5a0a1eaaeebc104047020
Akihiro Nakamura <akinaka.1428@gmail.com>
2018-04-07 14:57:29 +0900

    ほげ

--- Assets/Resources/GameBehavior.cs
+++ Assets/Resources/GameBehavior.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 //using HoloToolkit.Unity.InputModule;
 using UnityEngine.XR.WSA.Input;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
 
 public class GameBehavior : MonoBehaviour {
 
@@ -8,9 +10,11 @@ public class GameBehavior : MonoBehaviour {
     private GameObject TestBox;
     private GameObject cursor;
     private GestureRecognizer gestureRecognizer;
+    private Text text;
 
     // Use this for initialization
     void Start () {
+        text = GameObject.Find("Text").gameObject.GetComponent<Text>();
         // load TestBoxPrefab
         _TestBox = (GameObject)Resources.Load("TestBox");
         // initialize Cursor
@@ -21,16 +25,29 @@ public class GameBehavior : MonoBehaviour {
         {
             Debug.Log("TappedEvent");
             // if be notificated from image processing, an object is generated
-            if (TestBox != null) Destroy(TestBox);
+            if (TestBox != null && Sum(data, height, width) == 21) Destroy(TestBox);
             TestBox = Instantiate(_TestBox, cursor.transform.position, new Quaternion());
         };
         gestureRecognizer.StartCapturingGestures();
 
     }
-	
+
+    int frame = 0;
+    int height = 2;
+    int width = 3;
+    int number = 0;
+    byte[] data = new byte[] { 1, 2, 3, 4, 5, 6 };
+    [DllImport("cpplib")]
+    public static extern int Sum(byte[] data, int height, int width);
 	// Update is called once per frame
 	void Update () {
-        //InteractionManager.
+        frame++;
+        if (frame > 100) {
+            ++number;
+            text.text = number.ToString();
+            Debug.Log(Sum(data, height, width));
+            frame = 0;
+        }
 	}
     
 }
